bf9eaf5544695666aeb36240939f48e65e4cb442
Kalordo <yohann.leperson@gmail.com>
2025-06-25 17:26:48 +0200

    Ajout de la classe UserManager pour gérer les utilisateurs et mise à jour de l'autoload pour inclure cette classe.

--- config/autoload.php
+++ config/autoload.php
@@ -4,3 +4,4 @@ require __DIR__ . '/../controllers/UserController.php';
 require __DIR__ . '/Router.php';
 require __DIR__ . '/../models/User.php';
 require __DIR__ . '/../managers/AbstractManager.php';
+require __DIR__ . '/../managers/UserManager.php';
---
+++ managers/UserManager.php
@@ -0,0 +1,7 @@
+<?php
+
+class UserManager extends AbstractManager {
+    public function __construct() {
+        
+    }
+}
