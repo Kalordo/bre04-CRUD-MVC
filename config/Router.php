ccd7c87084cf08d4e22c76229066660ea5e112a8
Kalordo <yohann.leperson@gmail.com>
2025-06-25 10:41:09 +0200

    Mise en place du routeur et ajout des templates de base pour les utilisateurs

---
+++ config/Router.php
@@ -0,0 +1,34 @@
+<?php
+
+class Router {
+    public function __construct() {
+    }
+
+    public function handleRequest(array $get) : void {
+        $controller = new UserController();
+        $route = $get["route"] ?? "";      
+        switch ($route) {
+            case "show_user":
+                //$controller->show();
+                break;
+            case "create_user":
+                //$controller->create();
+                break;
+            case "check_create_user":
+                //$controller->checkCreate();
+                break;
+            case "update_user":
+                //$controller->update();
+                break;
+            case "check_update_user":
+                //$controller->checkUpdate();
+                break;
+            case "delete_user":
+                //$controller->delete();
+                break;
+            default:
+                //$controller->list();
+                break;
+        }
+    }
+}
