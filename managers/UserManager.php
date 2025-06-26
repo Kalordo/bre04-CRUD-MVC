3aeba33f1f2ebd1249b31a4f752f7725c6c8d43f
Kalordo <yohann.leperson@gmail.com>
2025-06-26 12:14:16 +0200

    Ajout des méthodes findAll, findOne, create, update et delete dans la classe UserManager pour gérer les opérations CRUD sur les utilisateurs.

--- managers/UserManager.php
+++ managers/UserManager.php
@@ -2,6 +2,44 @@
 
 class UserManager extends AbstractManager {
     public function __construct() {
-        
+        parent::__construct();
+    }
+
+    public function findAll() {
+        $result = $this->db->query("SELECT * FROM users");
+        return $result->fetchAll(PDO::FETCH_ASSOC);
+    }
+    public function findOne(int $id) {
+        $result = $this->db->prepare("SELECT * FROM users WHERE id = :id");
+        $result->execute([':id' => $id]);
+        return $result->fetch(PDO::FETCH_ASSOC);
+    }
+    public function create(User $user): void {
+        $result = $this->db->prepare("
+            INSERT INTO users (email, first_name, last_name)
+            VALUES (:email, :first_name, :last_name)");
+        $result->execute([
+            ':email' => $user->getEmail(),
+            ':first_name' => $user->getFirstName(),
+            ':last_name' => $user->getLastName()
+        ]);
+    }
+    public function update(User $user): void {
+        $result = $this->db->prepare('
+            UPDATE users 
+            SET email = :email,
+                first_name = :first_name,
+                last_name = :last_name
+            WHERE id = :id
+        ');
+        $result->execute([
+            ':email' => $user->getEmail(),
+            ':first_name' => $user->getFirstName(),
+            ':last_name' => $user->getLastName()
+        ]);
+    }
+    public function delete(User $user): void {
+        $result = $this->db->prepare("DELETE FROM users WHERE id = :id");
+        $result->execute([':id'=> $user->getId()]);
     }
 }
